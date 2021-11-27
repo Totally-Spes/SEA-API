e2fafa963eceb0eb5d6647746cf24266350bb51c
Darius ENGLER <75498958+DariusEngler789@users.noreply.github.com>
2021-11-27 03:21:45 +0100

    modified endpoints

--- api.py
+++ api.py
@@ -3,40 +3,32 @@ from datetime import timezone
 import database
 from flask import Flask, request, jsonify
 
-
 class API:
-    def __init__(self,app):
-        self.app = Flask(__name__)
-        self.app.add_url_rule('/api', 'index', self.__index)  # / -> index
-        self.app.add_url_rule('/api/location', 'toto', view_func=self.__get_items_in_region)
+    def __init__(self, app: Flask):
+        self.app = app
+        self.app.add_url_rule('/api', 'index', self.__index)
+        self.app.add_url_rule('/api/location/get/<latitude>/<longitude>/<radius>', 'getlocation', self.__get_location)
+        self.app.add_url_rule('/api/location/set/<latitude>/<longitude>/<amount>', 'setlocation', self.__set_location)
 
     def __index(self):
         return 'Hello, World!'
 
-    def __get_items_in_region(self):
-        if 'latitude' in request.args:
-            latitude = int(request.args['id'])
-        else:
-            return 'Error: No id field provided. Please specify an id.'
-
-        if 'longitude' in request.args:
-            longitude = int(request.args['longitude'])
-        else:
-            return 'Error: No longitude field provided. Please specify a longitude.'
-
-        if 'radius' in request.args:
-            radius = int(request.args['radius'])
-        else:
-            radius = 10
-            
+    def __get_location(self, latitude, longitude, radius):
         db = database.Database()
         date = datetime.datetime.now(timezone.utc).timestamp()
-        db.insert(date, latitude, longitude, radius)
+        return jsonify(db.get_location(date, latitude, longitude, radius))
+
+        # locations = db.get_locations(date, latitude, longitude, radius)
+
+        return jsonify(locations)
+
+    def __set_location(self, latitude, longitude, amount):
+        db = database.Database()
+        date = datetime.datetime.now(timezone.utc).timestamp()
+        db.insert(date, latitude, longitude, amount)
+
         return "OK"
-        return 'Seafish '  + str(id)
 
     def run(self):
         self.app.run(debug=True)
 
-
-    
--- app.py
+++ app.py
@@ -1,28 +1,7 @@
-#! /usr/bin/env python3
-
 import api
 from flask import Flask
 
-app = Flask(__name__)
-API = api.API(app)
-API.run()
-
-@app.route("/")
-def root_main():
-    return "Hello, World!"
-
-@app.route("/test1/<test>")
-def root_test1(test):
-    return "hello " + test
-
-
-@app.route("/test2")
-def root_test2():
-    return "hello " + request.args.get("name")
-
-@app.route("/postpath/<latitude>/<longitude>/<amount>")
-def route_post_path(latitude, longitude, amount):
-    db = database.Database()
-    date = datetime.datetime.now(timezone.utc).timestamp()
-    db.insert(date, latitude, longitude, amount)
-    return "OK"
+if __name__ == "__main__":
+    app = Flask(__name__)
+    API = api.API(app)
+    API.run()
