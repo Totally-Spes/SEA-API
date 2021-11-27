2d528728170e44621ebd9efd34fc88eba87b1046
Darius ENGLER <75498958+DariusEngler789@users.noreply.github.com>
2021-11-27 02:36:56 +0100

    fix api.py

--- api.py
+++ api.py
@@ -1,19 +1,23 @@
 from flask import Flask
+import datetime
+from datetime import timezone
+import database
 
 class API:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.add_url_rule('/', 'index', self.index) # / -> index
-        self.app.add_url_rule('/<path:path>', 'index', self.seafish) # /<path:path> -> index
+    def __init__(self, app):
+        self.app = app
+        self.app.add_url_rule('/', 'index', self.index)
+        self.app.add_url_rule('/postpath/<latitude>/<longitude>/<amount>', 'route_post_path', self.route_post_path)
 
     def index(self): 
         return 'Hello, World!'
 
-    def seafish(self):
-        return 'Seafish'
+    def route_post_path(self, latitude, longitude, amount):
+        db = database.Database()
+        date = datetime.datetime.now(timezone.utc).timestamp()
+        db.insert(date, latitude, longitude, amount)
+        return "OK"
 
     def run(self):
         self.app.run(debug=True)
 
-
-    
--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-#! /usr/bin/env python3
-
-from flask import request
-import datetime
-from datetime import timezone
-import database
-
+import api
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def root_main():
-    return "Hello, World!"
-
-@app.route("/test1/<test>")
-def root_test1(test):
-    return "hello " + test
-
-
-@app.route("/test2")
-def root_test2():
-    return "hello " + request.args.get("name")
-
-@app.route("/postpath/<latitude>/<longitude>/<amount>")
-def route_post_path(latitude, longitude, amount):
-    db = database.Database()
-    date = datetime.datetime.now(timezone.utc).timestamp()
-    db.insert(date, latitude, longitude, amount)
-    return "OK"
+API = api.API(app)
+API.run()
